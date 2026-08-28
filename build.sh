14f14a39ac2fced0821c7b9bb036ec7b499e89ba
noor hesham <101483584+noorhesham9@users.noreply.github.com>
2026-08-28 21:43:09 +0300

    edit

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 echo ">>> Installing front-end dependencies..."
-npm install --prefix front-end
+npm install --prefix front-end --foreground-scripts
 echo ">>> Building front-end..."
 npm run build --prefix front-end
 echo ">>> Build complete."
